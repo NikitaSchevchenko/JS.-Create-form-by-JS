95753a2e0a329a9e70f328e166db2c85121ec73d
Nikita Shevchenko <nik2302vin@gmail.com>
2026-06-04 13:58:23 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -87,7 +87,11 @@ function createInputRadioWrapperOption(
     const labelContentPartTwo = document.createElement("small");
     labelContentPartTwo.textContent = labelPartTwo;
 
-    labelContent.append(labelContentPartOne,document.createElement("br"), labelContentPartTwo);
+    labelContent.append(
+        labelContentPartOne,
+        document.createElement("br"),
+        labelContentPartTwo,
+    );
     option.append(radioButton, label);
 }
 
@@ -115,13 +119,101 @@ checkBox.setAttribute("name", "squadhelp");
 
 const checkBoxLabel = document.createElement("label");
 checkBoxLabel.setAttribute("for", "squadhelp");
-checkBoxLabel.textContent = "Allow Squadhelp to send marketing/promotional offers from time to time";
+checkBoxLabel.textContent =
+    "Allow Squadhelp to send marketing/promotional offers from time to time";
 
-checkBoxWrapper.append(checkBox,checkBoxLabel);
+checkBoxWrapper.append(checkBox, checkBoxLabel);
 form.appendChild(checkBoxWrapper);
 
 const submitButton = document.createElement("button");
-submitButton.setAttribute("type","submit");
+submitButton.setAttribute("type", "submit");
 submitButton.textContent = "Create account";
 form.appendChild(submitButton);
 
+function checkRoleNotEmpty() {
+    const role = document.querySelector('input[name="role"]:checked');
+    let roleErrorMessage = document.querySelector("p.role-error");
+
+    if (!role) {
+        if (!roleErrorMessage) {
+            roleErrorMessage = document.createElement("p");
+            roleErrorMessage.className = "role-error";
+            roleErrorMessage.textContent =
+                "Please choose who you will be join as!";
+            roleErrorMessage.style.color = "red";
+            roleErrorMessage.style.fontWeight = 700;
+            modal.appendChild(roleErrorMessage);
+        }
+        return false;
+    } else {
+        if (roleErrorMessage) {
+            modal.removeChild(roleErrorMessage);
+        }
+    }
+    return true;
+}
+
+function checkInputsNotEmpty() {
+    if (!checkRoleNotEmpty()) {
+        return false;
+    }
+    let emptyDataErrorMessage = document.querySelector("p.emptydata-error");
+    const inputsValues = Array.from(
+        document.querySelectorAll(".input-text-wrapper > .row > input"),
+    ).map((el) => el.value);
+    const hasEmptyInputs = inputsValues.some(
+        (value) => !value || value.trim() === "",
+    );
+    if (hasEmptyInputs) {
+        if (!emptyDataErrorMessage) {
+            emptyDataErrorMessage = document.createElement("p");
+            emptyDataErrorMessage.className = "emptydata-error";
+            emptyDataErrorMessage.textContent = "Please fill all data";
+            emptyDataErrorMessage.style.color = "red";
+            emptyDataErrorMessage.style.fontWeight = 700;
+            emptyDataErrorMessage.style.margin = 5;
+            modal.appendChild(emptyDataErrorMessage);
+        }
+        return false;
+    }
+
+    if (emptyDataErrorMessage) {
+        modal.removeChild(emptyDataErrorMessage);
+    }
+    return true;
+}
+
+class Person {
+    constructor(data) {
+        Object.assign(this, data);
+    }
+}
+
+function collectProps(event) {
+    event.preventDefault();
+    if (!checkInputsNotEmpty()) {
+        return;
+    }
+    const user = {};
+    const inputs = document.querySelectorAll(
+        ".input-text-wrapper > .row > input:not([type = 'password'])",
+    );
+    inputs.forEach((el) => {
+        let inputName = el.getAttribute("placeholder");
+        const words = inputName.split(" ");
+        //Тут делаем преобразования строки в формат сamelCase:допустим когда у нас был Last name, оно превратиться в lastName
+        inputName =
+            words[0].toLowerCase() +
+            words
+                .slice(1)
+                .map(
+                    (word) =>
+                        word[0].toUpperCase() + word.slice(1).toLowerCase(),
+                );
+        user[inputName] = el.value;
+    });
+    const person = new Person(user);
+    localStorage.setItem(person.lastName, JSON.stringify(person));
+}
+
+submitButton.addEventListener("click", collectProps);
